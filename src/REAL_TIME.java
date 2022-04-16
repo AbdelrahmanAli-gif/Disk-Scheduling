e96a057e06017df03467ac8d0153c3c312567365
[Omar Khaled Mohy] <[omarkhaledm21@gmail.com]>
2022-04-16 19:08:41 +0200

    bug fixes

--- src/REAL_TIME.java
+++ src/REAL_TIME.java
@@ -16,14 +16,28 @@ public class REAL_TIME {
         Collections.sort(reqs);
         for(int i=0; i<reqs.size(); i++){
             int req_val = reqs.get(i);
-            System.out.print(req_val+" -> ");
+            if(i != reqs.size() - 1) {
+                System.out.print(req_val + " -> ");
+            }else{
+                System.out.println(req_val);
+            }
             sequence.add(req_val);
         }
         totalHeadMv = reqs.get(reqs.size() - 1);
+        System.out.println("Total Head Movement = "+totalHeadMv);
     }
 
     public void REAL_TIME_Start(){
         sequence = new ArrayList<>();
+        reqs.add(98);
+        reqs.add(183);
+        reqs.add(37);
+        reqs.add(122);
+        reqs.add(14);
+        reqs.add(124);
+        reqs.add(65);
+        reqs.add(67);
+
         REAL_TIME_Disk_Scheduling();
     }
 
