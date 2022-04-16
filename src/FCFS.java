aa3a31a5cb72e2c33e8fdcca958e19574a80d9c0
Vcelist <petoilluis@gmail.com>
2022-04-16 19:00:33 +0200

    fcfs

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class FCFS {
+    ArrayList<Integer> sequence;
+    private int headMovements;
+    private int currentHead;
+    public FCFS(){
+        sequence = new ArrayList<>();
+        headMovements = 0;
+        currentHead = StaticData.INITIAL_HEAD_POINTER;
+    }
     public void FCFS(int[] requests) {
-        
+        for(Integer request: requests){
+            sequence.add(currentHead);
+            headMovements += Math.abs(request - currentHead);
+            currentHead = request;
+        }
+        System.out.println(sequence);
+        System.out.println(headMovements);
+    }
+
+    public static void main(String[] args) {
+        int[] requests = {98, 183, 37, 122, 14, 124, 65, 67};
+        StaticData.INITIAL_HEAD_POINTER = 53;
+        FCFS fcfs = new FCFS();
+        fcfs.FCFS(requests);
     }
 }
